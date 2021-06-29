3426c5a8da6232816359a7522110dee02afe9ee4
JV <byjoel41@gmail.com>
2021-06-28 23:01:18 -0300

    iniciando JS 2

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let CDU = prompt("Ingrese la contraseña");
 //////////////////////////////////////////////////////////////////
 
 if(CDU == 16795) {
-    alert("La contraseña es correcta, puedes pasar.")
+    alert("La contraseña es correcta, puedes pasar.");
+    console.log("Mi server de Discord es este: https://discord.gg/x85HtQr8vK");
 }else{
-    alert("Su contraseña es incorrecta.")
+    alert("Su contraseña es incorrecta.");
+    console.log("la contraseña correcta es 16795")
 }
