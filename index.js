f8c4678d79b499c7a90ac6b6954f2bdcd324b659
JV <byjoel41@gmail.com>
2021-07-17 21:43:24 -0300

    projecto 5

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-//////////////////////////////////////////////////////////////////
-let CDU = prompt("Ingrese la contraseña");
-//////////////////////////////////////////////////////////////////
+let producto = prompt("¿Que producto quieres? 1 = death note | 2 = capa de Harry Potter | 3 = Varita de Sauco de Harry Potter")
+function Producto(nombre, precio, descripción) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.descripción = descripción;
+    this.comprar = function() {
+        console.log("Usted ha comprado " + this.nombre)
+        alert(`${this.nombre} se ha agregado al carrito `)
+    }
+}
 
-if(CDU == 16795) {
-    alert("La contraseña es correcta, puedes pasar.");
-    console.log("Mi server de Discord es este: https://discord.gg/x85HtQr8vK");
-}else{
-    alert("Su contraseña es incorrecta.");
-    console.log("la contraseña correcta es 16795")
+const Death_Note = new Producto("Death Note", "$300", "Death Note es un cuaderno con una tapa negra y tu nombre está grabado en él. ... Por lo general, a menos que se indique la causa de la muerte, el nombre de la persona que figura en este cuaderno morirá de un ataque cardíaco después de 40 segundos. Se recomienda usar solo en emergencias.")
+const Capa = new Producto("Capa de Harry Potter", "$500", "Con esta capa puedes hacerte invisible a todas las personas y animales. Utilizar en caso de emergencia")
+const Varita = new Producto("Varita de Sauco de Harry Potter", "$1500", "Esta es la varita más poderosa de la saga, con esta varita puedes hacer cualquier cosa. EN MUCHO CUIDADO, ES MUY FRAGIL")
+console.clear();
+if(producto == 1){
+    Death_Note.comprar();
+} else if(producto == 2){
+Capa.comprar();
+} else if(producto == 3){
+    Varita.comprar();
+} else{
+    Producto()
 }
